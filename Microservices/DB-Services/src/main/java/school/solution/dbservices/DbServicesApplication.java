2c0803befb33b61b5011967bea0f0ee9f9d10c22
toukirnaim08 <toukir08@gmail.com>
2021-07-21 19:43:56 +0930

    db service initialized

---
+++ Microservices/DB-Services/src/main/java/school/solution/dbservices/DbServicesApplication.java
@@ -0,0 +1,13 @@
+package school.solution.dbservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DbServicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DbServicesApplication.class, args);
+	}
+
+}
---
+++ Microservices/DB-Services/src/test/java/school/solution/dbservices/DbServicesApplicationTests.java
@@ -0,0 +1,13 @@
+package school.solution.dbservices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DbServicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
