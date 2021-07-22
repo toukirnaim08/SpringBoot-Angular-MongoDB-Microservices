9736951e729bea35b1db382f00cad1df72917391
toukirnaim08 <toukir08@gmail.com>
2021-07-22 16:29:12 +0930

    Student Service added

---
+++ Microservices/Student-Services/src/test/java/school/solution/studentservices/StudentServicesApplicationTests.java
@@ -0,0 +1,13 @@
+package school.solution.studentservices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentServicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
