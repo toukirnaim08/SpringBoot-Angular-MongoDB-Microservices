9b6a555478d315ed9438ba8dbc7644b1f630db61
toukirnaim08 <toukir08@gmail.com>
2021-07-25 02:41:55 +0930

    connection between teacher-service and db-service created

--- Microservices/Teacher-Services/src/main/java/school/solution/teacherservices/controller/TeacherController.java
+++ Microservices/Teacher-Services/src/main/java/school/solution/teacherservices/controller/TeacherController.java
@@ -1,15 +1,29 @@
 package school.solution.teacherservices.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 @RestController
-@RequestMapping("/teacher")
+@RequestMapping("api/v1/teacher")
 public class TeacherController {
+	private String dbRoot = "http://DB-SERVICES/api/v1/db/";
+
+	@Autowired
+	private RestTemplate restTemplate;
+
+	@GetMapping("/save")
+	public String saveInstructor(){
+		String Instructor = restTemplate.getForObject(dbRoot+"saveinstructor", String.class);
+		return Instructor;
+	}
+
 	@GetMapping("/all")
-	public String test(){
-		return "teacherAPI";
+	public String allInstructors(){
+		String allInstructor = restTemplate.getForObject(dbRoot+"allinstructors", String.class);
+		return allInstructor;
 	}
 
 }
